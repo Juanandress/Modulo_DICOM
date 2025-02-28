5d7f19fbba109784e487c2a59b10ef6557cba1d4
Juanandress <sjuanandres94@gmail.com>
2025-02-27 19:12:13 -0500

    Actualización valor unico para imágenes

--- transformacion.py
+++ transformacion.py
@@ -1,7 +1,11 @@
+import shutil
+
 import pydicom
 import numpy as np
 import os
 from PIL import Image
+from tqdm import tqdm
+from pathlib import Path
 
 def extraerDatosYMedatados(file_path, output_dir, index):
     # Cargar el archivo DICOM
@@ -12,18 +16,27 @@ def extraerDatosYMedatados(file_path, output_dir, index):
 
     # Guardar Metadatos en un  txt
     saveMetadata = os.path.join(output_dir, f"metadata_{index}.txt")
-    with open(saveMetadata, "w") as f:
+    with open(saveMetadata, "w", encoding="utf-8") as f:
         for key, value in metaData.items():
-            f.write(f"{key}: {value}\n")
+            f.write(f"{key}: {str(value)}\n")
     print(f"Metadatos guardados en: {saveMetadata}")
 
     # Extraer datos de imagen
     if hasattr(fileDicom, "pixel_array"):
-        pixel_array = fileDicom.pixel_array
+        pixel_array = getattr(fileDicom,"pixel_array",None)
+        if pixel_array is None:
+            print(f"El archivo dicom{index} no contiene datos de imagen o está corrupto")
+            return
+
 
         # Comprobar el rango de los píxeles antes de la normalización
         print(f"Imagen {index}: Valor mínimo de píxel:", np.min(pixel_array))
         print(f"Imagen {index}: Valor máximo de píxel:", np.max(pixel_array))
+
+        def obtenerValorUnico(valor):
+            if isinstance(valor, list):
+                return float(valor[0])
+            return float(valor)
 
         # Obtener valores de la ventana
         window_center = fileDicom.get('WindowCenter', None)
@@ -31,8 +44,8 @@ def extraerDatosYMedatados(file_path, output_dir, index):
 
         if window_center and window_width:
             # Ajustar la ventana
-            window_center = float(window_center)
-            window_width = float(window_width)
+            window_center = obtenerValorUnico(window_center)
+            window_width = obtenerValorUnico(window_width)
 
             # Aplicar la ventana
             lower_bound = window_center - window_width // 2
@@ -56,15 +69,25 @@ def extraerDatosYMedatados(file_path, output_dir, index):
 
 # Ruta del directorio con archivos DICOM y directorio de salida
 dicom_directory = "E:\ImagenesDICOM\listaImagenes"
-output_directory = r"E:\ResultadoDICOM"
-
+output_directory = Path("E:\ResultadoDICOM")
+if output_directory.exists():
+    for file in output_directory.iterdir():
+        if file.is_file() or file.is_symlink():
+            file.unlink() #Elimina un archivo o un acceso directo
+        elif file.is_dir():
+            shutil.rmtree(file)#Borra carpetas y su contenido
 # Crear el directorio de salida si no existe
-os.makedirs(output_directory, exist_ok=True)
+output_directory.mkdir(parents=True, exist_ok=True)
+
 
 # Listar todos los archivos DICOM en el directorio
 dicom_files = [f for f in os.listdir(dicom_directory) if f.endswith('.dcm')]
 
-# Procesar n archivos que deseemos, para este caso 5 a modo de prueba
-for index, dicom_file in enumerate(dicom_files[:5]):
+# Procesar n archivos que deseemos, limite configurable con numArchivos
+numArchivos= input("¿Cuántos archivos desea procesar? (Presione enter para procesar todos los archivos):")
+numArchivos= int(numArchivos) if numArchivos.isdigit() else None
+dicom_files = dicom_files[:numArchivos]if numArchivos else dicom_files
+
+for index, dicom_file in enumerate(tqdm(dicom_files, desc="Procesando archivos...")):
     dicom_file_path = os.path.join(dicom_directory, dicom_file)
     extraerDatosYMedatados(dicom_file_path, output_directory, index + 1)
