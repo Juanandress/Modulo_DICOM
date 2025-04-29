c4c07f2df88a304cb60b9182c2e773f096541f4b
ASUS-GERSA\gersa <gersaingalindez93@gmail.com>
2025-04-29 17:10:07 -0500

    Refactorización y mejora de estructura del código
    - Se modularizó el código para mejorar la legibilidad y la organización.
    - Se actualizaron las rutas de lectura y salida para una mejor gestión de archivos.
    - Se implementó una estructura de directorios más clara para almacenar imágenes y metadatos por separado.

--- transformacion.py
+++ transformacion.py
@@ -1,14 +1,13 @@
-import shutil
-
 import pydicom
 import numpy as np
 import os
 from PIL import Image
-from tqdm import tqdm
-from pathlib import Path
-from pydicom.multival import MultiValue
+import os
 
-def extraerDatosYMedatados(file_path, output_dir, index):
+#import matplotlib.pyplot as plt
+
+
+def extraerDatosYMedatados(file_path, output_meta, output_img, index):
     # Cargar el archivo DICOM
     fileDicom = pydicom.dcmread(file_path)
 
@@ -16,28 +15,21 @@ def extraerDatosYMedatados(file_path, output_dir, index):
     metaData = {elem.keyword: elem.value for elem in fileDicom if elem.keyword}
 
     # Guardar Metadatos en un  txt
-    saveMetadata = os.path.join(output_dir, f"metadata_{index}.txt")
-    with open(saveMetadata, "w", encoding="utf-8") as f:
+    saveMetadata = os.path.join(output_meta, f"metadata_{index}.txt")
+    with open(saveMetadata, "w") as f:
         for key, value in metaData.items():
-            f.write(f"{key}: {str(value)}\n")
+            f.write(f"{key}: {value}\n")
     print(f"Metadatos guardados en: {saveMetadata}")
+    extraerImagenJPG(fileDicom, output_img, index)
 
+def extraerImagenJPG(fileDicom, output_img, index):
     # Extraer datos de imagen
     if hasattr(fileDicom, "pixel_array"):
-        pixel_array = getattr(fileDicom,"pixel_array",None)
-        if pixel_array is None:
-            print(f"El archivo dicom{index} no contiene datos de imagen o está corrupto")
-            return
-
+        pixel_array = fileDicom.pixel_array
 
         # Comprobar el rango de los píxeles antes de la normalización
         print(f"Imagen {index}: Valor mínimo de píxel:", np.min(pixel_array))
         print(f"Imagen {index}: Valor máximo de píxel:", np.max(pixel_array))
-
-        def obtenerValorUnico(valor):
-            if isinstance(valor, MultiValue):
-                return float(valor[0])
-            return float(valor)
 
         # Obtener valores de la ventana
         window_center = fileDicom.get('WindowCenter', None)
@@ -45,8 +37,8 @@ def extraerDatosYMedatados(file_path, output_dir, index):
 
         if window_center and window_width:
             # Ajustar la ventana
-            window_center = obtenerValorUnico(window_center)
-            window_width = obtenerValorUnico(window_width)
+            window_center = float(window_center)
+            window_width = float(window_width)
 
             # Aplicar la ventana
             lower_bound = window_center - window_width // 2
@@ -59,37 +51,35 @@ def extraerDatosYMedatados(file_path, output_dir, index):
             pixel_array = (pixel_array * 255).astype(np.uint8)
 
         # Crear la imagen desde el array de píxeles
-        image = Image.fromarray(pixel_array)
+        #image = Image.fromarray(pixel_array)
+        image = Image.fromarray(pixel_array).convert('L')  # 'L' fuerza la escala de grises
 
-        # Guardar la imagen
-        image_file = os.path.join(output_dir, f"image_{index}.jpg")
+        # Guardar la imagen como PNG
+        image_file = os.path.join(output_img, f"image_{index}.jpg")
         image.save(image_file)
         print(f"Imagen {index} guardada en: {image_file}")
     else:
         print(f"El archivo DICOM {index} no contiene datos de imagen.")
 
-# Ruta del directorio con archivos DICOM y directorio de salida
-dicom_directory = "D:/ImagenesDICOM/listaImagenes"
-output_directory = Path("D:/ResultadoDICOM")
-if output_directory.exists():
-    for file in output_directory.iterdir():
-        if file.is_file() or file.is_symlink():
-            file.unlink() #Elimina un archivo o un acceso directo
-        elif file.is_dir():
-            shutil.rmtree(file)#Borra carpetas y su contenido
-
-# Crear el directorio de salida si no existe
-output_directory.mkdir(parents=True, exist_ok=True)
+def main():
+    """ Función principal para procesar archivos DICOM en un directorio. """
+    dicom_directory = "Modulo_DICOM\ArchivosDICOM"
+    # Obtener la ruta del directorio donde se encuentra el script
+    project_root = os.path.dirname(os.path.abspath(__file__))  # 
+    output_metadata = os.path.join(project_root, "ResultadoDICOM", "metadata")
+    output_images = os.path.join(project_root, "ResultadoDICOM", "images")
 
 
-# Listar todos los archivos DICOM en el directorio
-dicom_files = [f for f in os.listdir(dicom_directory) if f.endswith('.dcm')]
+    # Crear ambos directorios si no existen
+    os.makedirs(output_metadata, exist_ok=True)
+    os.makedirs(output_images, exist_ok=True)
 
-# Procesar n archivos que deseemos, limite configurable con numArchivos
-numArchivos= input("¿Cuántos archivos desea procesar? (Presione enter para procesar todos los archivos):")
-numArchivos= int(numArchivos) if numArchivos.isdigit() else None
-dicom_files = dicom_files[:numArchivos]if numArchivos else dicom_files
+    dicom_files = [f for f in os.listdir(dicom_directory) if f.endswith('.dcm')]
 
-for index, dicom_file in enumerate(tqdm(dicom_files, desc="Procesando archivos...")):
-    dicom_file_path = os.path.join(dicom_directory, dicom_file)
-    extraerDatosYMedatados(dicom_file_path, output_directory, index + 1)
+    # Procesar n archivos que deseemos, para este caso 5 a modo de prueba
+    for index, dicom_file in enumerate(dicom_files[:15]):
+        dicom_file_path = os.path.join(dicom_directory, dicom_file)
+        extraerDatosYMedatados(dicom_file_path, output_metadata, output_images, index + 1)
+
+if __name__ == "__main__":
+    main()
